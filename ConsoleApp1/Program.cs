f2b88369e47832e395105b9ce72f11e6e17f305a
BohdanNikolaychuk <nikolaychukbm@gmail.com>
2020-02-28 21:27:55 +0200

    add

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        static public int Example(int n)
+        {
+            int m = n / 60;
+            Console.WriteLine(m.ToString() + " секунд");
+            return m;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введiть перше число");
+            string S1 = Console.ReadLine();
+            int n = int.Parse(S1);
+            Example(n);
+            Console.ReadLine();
+        }
+    }
+
+}
